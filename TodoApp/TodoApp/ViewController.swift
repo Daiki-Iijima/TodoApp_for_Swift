0961225421060ddca411e88b6e9894981e362433
Daiki-Iijima <daiki.applecreate@gmail.com>
2019-11-09 02:55:27 +0900

    コメント設計追加

--- TodoApp/TodoApp/ViewController.swift
+++ TodoApp/TodoApp/ViewController.swift
@@ -8,13 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    var todoList = [String]();
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    //  メモリ不足になった時に呼ばれるメソッド
+    //  前のXcodeのバージョン(9?)までは自動生成されていた
+    //  今は自動で生成されない
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    //  +ボタンをタップした時の処理
+    @IBAction func tapAddButton(_ sender: Any) {
+    
+        //  アラートのダイアログを生成
+        
+        //  テキストエリアを追加
+        
+        //  OKボタンを追加
+        
+        //  OKボタンがタップされた時の処理
+        
+        //  ToDoの配列に入力された値を挿入
+        
+        //  テーブルに行が追加されたことをテーブルに通知
+        
+        //  OKボタンがタップされた時の処理
+        
+        //  CANCELボタンが押された時の処理
+        
+        //  CANCELボタンを追加
+        
+        //  アラートダイアログを表示
+    }
+    
+    
+    
 }
 
